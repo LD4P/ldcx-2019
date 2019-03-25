104007510708c25b88ab4eb16569f46916bd902e
Jeremy Nelson <jermnelson@gmail.com>
2019-03-25 13:40:44 -0700

    Updates

---
+++ precache-manifest.476fe05b3f46db43da87c9820e2f1b37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b62e74f0c408aa3c9a20fc0e71b16e9a",
+    "url": "/ldcx-2019/static/media/sinopiahomebg.b62e74f0.png"
+  },
+  {
+    "revision": "aeb0eff2e78f0974b22a",
+    "url": "/ldcx-2019/static/js/runtime~main.bb7d8b52.js"
+  },
+  {
+    "revision": "d6b67040a4fbb08dd390",
+    "url": "/ldcx-2019/static/js/main.71d31171.chunk.js"
+  },
+  {
+    "revision": "44461e975102ef37129b",
+    "url": "/ldcx-2019/static/js/2.716bab56.chunk.js"
+  },
+  {
+    "revision": "d6b67040a4fbb08dd390",
+    "url": "/ldcx-2019/static/css/main.63215634.chunk.css"
+  },
+  {
+    "revision": "dcf0db27440f7ae0e4a65804b10e99b4",
+    "url": "/ldcx-2019/index.html"
+  }
+];
